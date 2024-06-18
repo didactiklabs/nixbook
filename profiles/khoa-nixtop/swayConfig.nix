9618cf13e5c594dbe1faa3533da9966c7b55cc97
Victor Hang <vhvictorhang@gmail.com>
2024-06-18 10:09:29 +0200

    feat: sunshine nixtop

--- profiles/khoa-nixtop/swayConfig.nix
+++ profiles/khoa-nixtop/swayConfig.nix
@@ -16,5 +16,9 @@ in {
       exec ${swaymsg} create_output HEADLESS-1
       exec ${sunshine}
     '';
+    extraSessionCommands = ''
+      export WLR_BACKENDS="headless,libinput"
+      export WLR_LIBINPUT_NO_DEVICES="1"
+    '';
   };
 }
