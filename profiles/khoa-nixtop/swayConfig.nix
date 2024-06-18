be5f950ab1e65450c1335bc4847c75b58aecce72
Victor Hang <vhvictorhang@gmail.com>
2024-06-18 10:42:00 +0200

    feat: sunshine nixtop

--- profiles/khoa-nixtop/swayConfig.nix
+++ profiles/khoa-nixtop/swayConfig.nix
@@ -17,6 +17,7 @@ in {
       };
     wayland.windowManager.sway.extraConfig = ''
       exec ${swaymsg} create_output HEADLESS-1
+      exec ${swaymsg} output HEADLESS-1 mode 3840x2160 position 5000,2000
       exec ${sunshine}
     '';
     wayland.windowManager.sway.extraSessionCommands = ''
