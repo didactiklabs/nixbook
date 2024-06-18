44694930e95dabdaebc05e0cb8120ba0f7dec40d
Victor Hang <vhvictorhang@gmail.com>
2024-06-18 09:55:45 +0200

    feat: sunshine nixtop

--- profiles/khoa-nixtop/swayConfig.nix
+++ profiles/khoa-nixtop/swayConfig.nix
@@ -11,7 +11,7 @@ in {
     wayland.windowManager.sway.config.keybindings =
       lib.filterAttrsRecursive (name: value: value != null) {
       };
-    wayland.windowManager.sway.extraConfig = ''
+    wayland.windowManager.sway.config.extraConfig = ''
       ${swaymsg} create_output HEADLESS-1
     '';
   };
