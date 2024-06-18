938491e5380d397dc185eb31084507e31d3a209b
Victor Hang <vhvictorhang@gmail.com>
2024-06-18 11:04:19 +0200

    feat: sunshine nixtop

--- homeManagerModules/sway/swayConfig.nix
+++ homeManagerModules/sway/swayConfig.nix
@@ -190,6 +190,7 @@ in {
         export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
         export _JAVA_AWT_WM_NONREPARENTING="1"
         export MOZ_ENABLE_WAYLAND="1"
+        export DESKTOP_SESSION="sway"
         export XDG_SESSION_TYPE="wayland"
         export XDG_SESSION_DESKTOP="sway"
         export XDG_CURRENT_DESKTOP="sway"
