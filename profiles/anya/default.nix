695505e6c0160be370886fc51aedf7a13419c9f4
Victor Hang <vhvictorhang@gmail.com>
2024-08-31 11:44:33 +0200

    feat(anya): fix immich upload

--- profiles/anya/default.nix
+++ profiles/anya/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, sources, ... }:
+{ pkgs, pkgs-unstable, lib, sources, ... }:
 let
   mainIf = "enp34s0";
   overrides = {
@@ -19,7 +19,7 @@ in {
       description = "Run my command";
       serviceConfig = {
         ExecStart =
-          "${pkgs.bash}/bin/bash -c '${pkgs.immich-go}/bin/immich-go -no-ui -key $(cat /home/khoa/.immich-token) -server https://photos.didactiklabs.io upload /home/khoa/.steam/steam/steamapps/compatdata/1091500/pfx/drive_c/users/steamuser/Pictures/Cyberpunk\\ 2077/ && ${pkgs.coreutils}/bin/rm -fr /home/khoa/.steam/steam/steamapps/compatdata/1091500/pfx/drive_c/users/steamuser/Pictures/Cyberpunk\\ 2077/*'";
+          "${pkgs.bash}/bin/bash -c '${pkgs-unstable.immich-go}/bin/immich-go -no-ui -key $(cat /home/khoa/.immich-token) -server https://photos.didactiklabs.io upload /home/khoa/.steam/steam/steamapps/compatdata/1091500/pfx/drive_c/users/steamuser/Pictures/Cyberpunk\\ 2077/ && ${pkgs.coreutils}/bin/rm -fr /home/khoa/.steam/steam/steamapps/compatdata/1091500/pfx/drive_c/users/steamuser/Pictures/Cyberpunk\\ 2077/*'";
       };
     };
 
