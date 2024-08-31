14942cb179b4234449ab5f209ffed67c46d303b0
Victor Hang <vhvictorhang@gmail.com>
2024-09-01 00:12:04 +0200

    feat ✨(totoro): add vpn, and service, auto connect
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/khoa/config.nix
+++ profiles/totoro/khoa/config.nix
@@ -1,21 +1,41 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let bealvVpnConf = ../../../assets/openvpn/bealv.ovpn;
+in {
   config = {
-    systemd.user.services.jellyfin-mpv-shim = {
-      Unit = { Description = "Jellyfin mpv"; };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-      Unit = { After = [ "graphical-session.target" ]; };
-      Service = {
-        ExecStart = "${pkgs.jellyfin-mpv-shim}/bin/jellyfin-mpv-shim";
-        Restart = "always";
-      };
-    };
-    systemd.user.services.nextcloud-client = {
-      Unit = { Description = "Nextcloud"; };
-      Install = { WantedBy = [ "graphical-session.target" ]; };
-      Unit = { After = [ "graphical-session.target" ]; };
-      Service = {
-        ExecStart = "${pkgs.nextcloud-client}/bin/nextcloud";
-        Restart = "always";
+    systemd = {
+      user = {
+        services = {
+          bealvVpn = {
+            Unit = {
+              Description = "Bealv VPN";
+              After = [ "network-online.target" ];
+            };
+            Install = { WantedBy = [ "network-online.target" ]; };
+            Service = {
+              ExecStart =
+                "${pkgs.bash}/bin/bash -c 'sudo -E ${pkgs.openvpn}/bin/openvpn --up ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --down ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --config ${bealvVpnConf} --auth-user-pass $HOME/.bealv_vpn_pass --cd /tmp'";
+              Restart = "always";
+            };
+          };
+          nextcloud-client = {
+            Unit = { Description = "Nextcloud"; };
+            Install = { WantedBy = [ "graphical-session.target" ]; };
+            Unit = { After = [ "graphical-session.target" ]; };
+            Service = {
+              ExecStart = "${pkgs.nextcloud-client}/bin/nextcloud";
+              Restart = "always";
+            };
+          };
+          jellyfin-mpv-shim = {
+            Unit = { Description = "Jellyfin mpv"; };
+            Install = { WantedBy = [ "graphical-session.target" ]; };
+            Unit = { After = [ "graphical-session.target" ]; };
+            Service = {
+              ExecStart = "${pkgs.jellyfin-mpv-shim}/bin/jellyfin-mpv-shim";
+              Restart = "always";
+            };
+          };
+        };
       };
     };
   };
