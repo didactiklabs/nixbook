436bec1484830735048e2c638b109e206b44ac67
Victor Hang <vhvictorhang@gmail.com>
2024-09-02 23:01:33 +0200

    feat ✨(github): add gh cli
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- homeManagerModules/gitConfig.nix
+++ homeManagerModules/gitConfig.nix
@@ -5,6 +5,8 @@
   ...
 }:
 let
+  sources = import ../npins;
+  pkgs-unstable = import sources.nixpkgs-unstable { };
   cfg = config.customHomeManagerModules.gitConfig;
 in
 {
@@ -25,48 +27,92 @@ in
       ## https://difftastic.wilfred.me.uk/git.html
       pkgs.difftastic
     ];
-
-    programs.git = {
-      package = pkgs.gitFull;
-      enable = true;
-      signing = {
-        signByDefault = false;
-        gpgPath = "${pkgs.gnupg}/bin/gpg2";
-        key = null;
+    programs = {
+      gh = {
+        enable = true;
+        package = pkgs-unstable.gh;
+        extensions = with pkgs-unstable; [
+          gh-eco
+          gh-notify
+          gh-poi
+          gh-f
+        ];
       };
-      lfs.enable = true;
-      difftastic.enable = true;
-      ignores = [
-        "*.vscode"
-        "*.direnv"
-      ];
-      aliases = {
-        lg = "log --graph --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ai)%Creset'";
-        d = "diff";
-        s = "status";
-        sw = "switch";
-        swcr = "switch -C";
-        del = "branch -D";
-        br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
-        save = "!git add -A && git commit -m 'chore: commit save point'";
-        undo = "reset HEAD~1 --mixed";
-        res = "!git reset --hard";
-        done = "!git push origin HEAD";
-        lazy = ''!f() { git add -A && git commit -m "$@" && git push; }; f'';
-        pushmr = ''!f() { git pull && git checkout -b "$@" && git add -A && git commit -m "$@" && git push origin "$@"; }; f'';
-        purge = ''!git branch --merged | egrep -v "(^\*|master|main|dev|stage)" | xargs git branch -d'';
+      gh-dash = {
+        enable = true;
+        settings = {
+          prSections = [
+            {
+              title = "My Pull Requests";
+              filters = "is:open author:@me";
+            }
+            {
+              title = "Needs My Review";
+              filters = "is:open review-requested:@me";
+            }
+            {
+              title = "Participating";
+              filters = "is:open involves:@me -author:@me";
+            }
+          ];
+          issuesSections = [
+            {
+              title = "My Issues";
+              filters = "is:open author:@me";
+            }
+            {
+              title = "Assigned";
+              filters = "is:open assignee:@me";
+            }
+            {
+              title = "Participating";
+              filters = "is:open involves:@me -author:@me";
+            }
+          ];
+        };
       };
-      ## https://nix-community.github.io/home-manager/options.html#opt-programs.git.extraConfig
-      extraConfig = {
-        pull.rebase = true;
-        init = {
-          defaultBranch = "main";
+      git = {
+        package = pkgs.gitFull;
+        enable = true;
+        signing = {
+          signByDefault = false;
+          gpgPath = "${pkgs.gnupg}/bin/gpg2";
+          key = null;
         };
-        core = {
-          excludesFile = "";
+        lfs.enable = true;
+        difftastic.enable = true;
+        ignores = [
+          "*.vscode"
+          "*.direnv"
+        ];
+        aliases = {
+          lg = "log --graph --pretty=tformat:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%an %ai)%Creset'";
+          d = "diff";
+          s = "status";
+          sw = "switch";
+          swcr = "switch -C";
+          del = "branch -D";
+          br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
+          save = "!git add -A && git commit -m 'chore: commit save point'";
+          undo = "reset HEAD~1 --mixed";
+          res = "!git reset --hard";
+          done = "!git push origin HEAD";
+          lazy = ''!f() { git add -A && git commit -m "$@" && git push; }; f'';
+          pushmr = ''!f() { git pull && git checkout -b "$@" && git add -A && git commit -m "$@" && git push origin "$@"; }; f'';
+          purge = ''!git branch --merged | egrep -v "(^\*|master|main|dev|stage)" | xargs git branch -d'';
         };
-        remote = {
-          prune = true;
+        ## https://nix-community.github.io/home-manager/options.html#opt-programs.git.extraConfig
+        extraConfig = {
+          pull.rebase = true;
+          init = {
+            defaultBranch = "main";
+          };
+          core = {
+            excludesFile = "";
+          };
+          remote = {
+            prune = true;
+          };
         };
       };
     };
--- profiles/totoro/khoa/goreadConfig.nix
+++ profiles/totoro/khoa/goreadConfig.nix
@@ -7,12 +7,24 @@ let
           - name: ArsTechnica
             desc: News from ArsTechnica
             url: https://feeds.arstechnica.com/arstechnica/index
-      - name: IT Tech
+      - name: IT Tech French
         desc: Tech, IT stuffs
         subscriptions:
           - name: Zwindler Blog
             desc: News from Zwindler
             url: https://blog.zwindler.fr/index.xml
+          - name: Korben Blog
+            desc: News from Korben
+            url: https://korben.info/feed
+          - name: FrenchWeb
+            desc: News from FrenchWeb
+            url: https://www.frenchweb.fr/feed
+          - name: FredZone
+            desc: News from FredZone
+            url: https://feeds.feedburner.com/Fredzone
+      - name: IT Tech
+        desc: Tech, IT stuffs
+        subscriptions:
           - name: Hacker News
             desc: Hacker News
             url: https://hnrss.org/frontpage
@@ -22,12 +34,24 @@ let
           - name: Terminal Trove
             desc: News from Terminal Trove
             url: https://terminaltrove.com/new.xml
+          - name: r/commandline
+            desc: News from r/commandline
+            url: https://old.reddit.com/r/commandline/.rss
           - name: r/selfhosted
             desc: News from r/selfhosted
             url: https://old.reddit.com/r/selfhosted/.rss
           - name: r/coolgithubprojects
             desc: News from r/coolgithubprojects
             url: https://old.reddit.com/r/coolgithubprojects/.rss
+      - name: Anime French
+        desc: Anime News
+        subscriptions:
+          - name: Japananime News
+            desc: News from Japananime
+            url: https://www.japananime.fr/feed
+          - name: Animeland News
+            desc: News from Animeland
+            url: https://animeland.fr/feed
       - name: Anime
         desc: Anime News
         subscriptions:
@@ -37,7 +61,15 @@ let
           - name: MyAnimeList News
             desc: News from MyAnimeList
             url: https://myanimelist.net/rss/news.xml
-
+          - name: AlltheAnime News
+            desc: News from AlltheAnime
+            url: https://blog.alltheanime.com/feed
+      - name: Gaming
+        desc: Gaming News
+        subscriptions:
+          - name: Valve News
+            desc: News from Valve
+            url: https://store.steampowered.com/feeds/news/
   '';
 in
 {
