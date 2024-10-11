ee048d020da37ac3be49454aee906300e40a831a
Victor Hang <vhvictorhang@gmail.com>
2024-10-11 23:34:41 +0200

    chore 🧹(hyprland): hyprland cachix
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nixosModules/hyprland.nix
+++ nixosModules/hyprland.nix
@@ -17,6 +17,10 @@ in
       };
     };
     security.pam.services.hyprlock = { };
+    nix.settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
   };
   options.customNixOSModules.hyprland = {
     enable = lib.mkOption {
