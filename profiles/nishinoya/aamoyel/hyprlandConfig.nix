39f5183225466ac65385da5c49d730836a7f9cdb
Alan Amoyel <alanamoyel06@gmail.com>
2024-12-27 17:57:37 +0100

    chore 🧹(profile): update nishinoya settings
    Signed-off-by: Alan Amoyel <alanamoyel06@gmail.com>

--- profiles/nishinoya/aamoyel/hyprlandConfig.nix
+++ profiles/nishinoya/aamoyel/hyprlandConfig.nix
@@ -7,11 +7,9 @@ in
     wayland.windowManager.hyprland.settings = {
       monitor = lib.mkForce [
         ",preferred,auto,1"
-        "eDP-1,preferred,0x587,1.0"
-        "DP-8,1920x1080,1440x0,auto"
-        "DP-9,1920x1080,3360x0,auto"
-        "DP-10,1920x1080,1440x0,auto"
-        "DP-11,1920x1080,3360x0,auto"
+        "eDP-1,preferred,3000x0,1.666667"
+        "DP-10,1920x1080,1080x0,1.0"
+        "DP-9,1920x1080,0x0,1.0, transform, 1"
       ];
       windowrulev2 = [
         "workspace 1 silent,class:(vesktop)"
