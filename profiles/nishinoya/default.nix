e7b92031ff3bd788b19cf072c92f5e5d57f20303
Alan Amoyel <alanamoyel06@gmail.com>
2024-12-29 01:26:28 +0100

    feat ✨(lock): add yubikey to nishinoya
    Signed-off-by: Alan Amoyel <alanamoyel06@gmail.com>

--- profiles/nishinoya/default.nix
+++ profiles/nishinoya/default.nix
@@ -19,6 +19,14 @@ let
   };
 in
 {
+  services.udev.extraRules = ''
+    ACTION=="remove",\
+     ENV{ID_BUS}=="usb",\
+     ENV{ID_MODEL_ID}=="0407",\
+     ENV{ID_VENDOR_ID}=="1050",\
+     ENV{ID_VENDOR}=="Yubico",\
+     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
   customNixOSModules = {
     workTools.enable = true;
     laptopProfile.enable = true;
