25a059bdf528d23fa67a0e758092d5dbbb720274
Victor Hang <vhvictorhang@gmail.com>
2025-01-02 10:37:26 +0100

    fix 🐛: fix tailscale parasiting local subnetting 2
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/default.nix
+++ profiles/totoro/default.nix
@@ -19,8 +19,8 @@ let
   };
   tailscale-fix-routes = pkgs.writeShellScriptBin "tailscale-fix-routes" ''
     set -euo pipefail
-    SUBNET=$(ip -4 route show default | awk '{print $3}' | cut -d. -f1-3).0/16
     ip monitor route | while read -r line; do
+        SUBNET=$(ip -4 route show default | awk '{print $3}' | cut -d. -f1-3).0/16
         if echo "$line" | grep -q "$SUBNET dev tailscale0"; then
             if ip route show table 52 | grep -q "$SUBNET dev tailscale0"; then
                 ip route del $SUBNET dev tailscale0 table 52
