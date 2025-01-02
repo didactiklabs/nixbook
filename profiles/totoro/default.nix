1519733ebb754b35215e275355f2aee795979115
Victor Hang <vhvictorhang@gmail.com>
2025-01-02 03:00:28 +0100

    fix 🐛: fix tailscale parasiting local subnetting
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/default.nix
+++ profiles/totoro/default.nix
@@ -17,8 +17,38 @@ let
       overrides
       ;
   };
+  tailscale-fix-routes = pkgs.writeShellScriptBin "tailscale-fix-routes" ''
+    set -euo pipefail
+    SUBNET=$(ip -4 route show default | awk '{print $3}' | cut -d. -f1-3).0/16
+    ip monitor route | while read -r line; do
+        if echo "$line" | grep -q "$SUBNET dev tailscale0"; then
+            if ip route show table 52 | grep -q "$SUBNET dev tailscale0"; then
+                ip route del $SUBNET dev tailscale0 table 52
+            fi
+        fi
+    done
+  '';
 in
 {
+  environment = {
+    systemPackages = [
+      tailscale-fix-routes
+    ];
+  };
+  systemd = {
+    services.tailscale-fix-routes = {
+      enable = true;
+      path = [
+        pkgs.iproute2
+        pkgs.busybox
+      ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${tailscale-fix-routes}/bin/tailscale-fix-routes";
+        Restart = "always";
+      };
+    };
+  };
   networking.hosts = {
     "100.111.17.126" = [
       "gitea"
