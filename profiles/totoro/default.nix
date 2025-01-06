9c2e33e9a5d33fd4e7194578c244d18c30b61318
Victor Hang <vhvictorhang@gmail.com>
2025-01-06 15:51:53 +0100

    fix 🐛: tailscale fix global
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/default.nix
+++ profiles/totoro/default.nix
@@ -17,38 +17,8 @@ let
       overrides
       ;
   };
-  tailscale-fix-routes = pkgs.writeShellScriptBin "tailscale-fix-routes" ''
-    set -euo pipefail
-    ip monitor route | while read -r line; do
-        SUBNET=$(ip -4 route show default | awk '{print $3}' | cut -d. -f1-3).0/16
-        if echo "$line" | grep -q "$SUBNET dev tailscale0"; then
-            if ip route show table 52 | grep -q "$SUBNET dev tailscale0"; then
-                ip route del $SUBNET dev tailscale0 table 52
-            fi
-        fi
-    done
-  '';
 in
 {
-  environment = {
-    systemPackages = [
-      tailscale-fix-routes
-    ];
-  };
-  systemd = {
-    services.tailscale-fix-routes = {
-      enable = true;
-      path = [
-        pkgs.iproute2
-        pkgs.busybox
-      ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${tailscale-fix-routes}/bin/tailscale-fix-routes";
-        Restart = "always";
-      };
-    };
-  };
   networking.hosts = {
     "100.111.17.126" = [
       "gitea"
