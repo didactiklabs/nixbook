db646cfeb611bfff6dd8197d0e7915a9d720c016
Victor Hang <vhvictorhang@gmail.com>
2025-01-15 20:51:51 +0100

    feat ✨: add tailscale switch script
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nixosModules/tailscale-fix.nix
+++ nixosModules/tailscale-fix.nix
@@ -1,5 +1,27 @@
 { pkgs, ... }:
 let
+  tailscale-switch = pkgs.writeShellScriptBin "tswitch" ''
+    # Get the list of available Tailnets
+    tailnet_list=$(tailscale switch --list | tail -n +2 2>/dev/null)
+
+    # Use fzf to select a Tailnet
+    selected_tailnet=$(echo "$tailnet_list" | fzf --prompt="Select a Tailnet> ")
+
+    if [[ -z "$selected_tailnet" ]]; then
+      echo "No Tailnet selected. Exiting."
+      exit 1
+    fi
+
+    tailnet_name=$(echo "$selected_tailnet" | awk '{gsub("\\*", "", $3); print $3}')
+    echo "$tailnet_name"
+    echo "Switching to Tailnet: $tailnet_name..."
+
+    if tailscale switch "$tailnet_name"; then
+      echo "Successfully switched to $tailnet_name."
+    else
+      echo "Failed to switch to $tailnet_name."
+    fi
+  '';
   tailscale-fix-routes = pkgs.writeShellScriptBin "tailscale-fix-routes" ''
     set -euo pipefail
     ip monitor route | while read -r line; do
@@ -15,6 +37,11 @@ let
   '';
 in
 {
+  environment = {
+    systemPackages = [
+      tailscale-switch
+    ];
+  };
   systemd = {
     services.tailscale-fix-routes = {
       enable = true;
