a652c2797b262e6168e3e25f17c88c888fa5ddb7
Victor Hang <vhvictorhang@gmail.com>
2025-01-16 00:22:09 +0100

    chore 🧹: remove bealv opnvpn
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/khoa/config.nix
+++ profiles/totoro/khoa/config.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }:
-let
-  bealvVpnConf = ../../../assets/openvpn/bealv.ovpn;
-in
 {
   config = {
     systemd = {
@@ -49,25 +46,25 @@ in
               Restart = "always";
             };
           };
-          bealvVpn = {
-            Unit = {
-              Description = "Bealv VPN";
-              After = [
-                "graphical-session.target"
-                "network-online.target"
-              ];
-            };
-            Install = {
-              WantedBy = [
-                "graphical-session.target"
-                "network-online.target"
-              ];
-            };
-            Service = {
-              ExecStart = "${pkgs.bash}/bin/bash -c 'sudo -E ${pkgs.openvpn}/bin/openvpn --up ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --down ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --config ${bealvVpnConf} --auth-user-pass $HOME/.bealv_vpn_pass --cd /tmp'";
-              Restart = "always";
-            };
-          };
+          # bealvVpn = {
+          #   Unit = {
+          #     Description = "Bealv VPN";
+          #     After = [
+          #       "graphical-session.target"
+          #       "network-online.target"
+          #     ];
+          #   };
+          #   Install = {
+          #     WantedBy = [
+          #       "graphical-session.target"
+          #       "network-online.target"
+          #     ];
+          #   };
+          #   Service = {
+          #     ExecStart = "${pkgs.bash}/bin/bash -c 'sudo -E ${pkgs.openvpn}/bin/openvpn --up ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --down ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --config ${bealvVpnConf} --auth-user-pass $HOME/.bealv_vpn_pass --cd /tmp'";
+          #     Restart = "always";
+          #   };
+          # };
         };
       };
     };
