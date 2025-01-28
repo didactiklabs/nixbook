9f1e754abbfe34fc6f64ccfced1ed6bd16df4514
Victor Hang <vhvictorhang@gmail.com>
2025-01-28 15:54:28 +0100

    feat ✨: allow disable spotify
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- homeManagerModules/spicetifyConfig.nix
+++ homeManagerModules/spicetifyConfig.nix
@@ -6,14 +6,23 @@
 }:
 let
   sources = import ../npins;
-  flake-compat = sources.flake-compat;
+  inherit (sources) flake-compat;
   spicetify-nix = (import flake-compat { src = sources.spicetify-nix; }).defaultNix;
   spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
   cfg = config.customHomeManagerModules;
 in
 {
+  options.customHomeManagerModules.spicetifyConfig = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        whether to enable desktopApps globally or not
+      '';
+    };
+  };
   imports = [ spicetify-nix.homeManagerModules.default ];
-  config = lib.mkIf cfg.desktopApps.enable {
+  config = lib.mkIf cfg.spicetifyConfig.enable {
     programs.spicetify = {
       enable = true;
       spotifyPackage = pkgs.spotify;
