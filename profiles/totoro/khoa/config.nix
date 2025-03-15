760da6c57752006c3cfcbe635d30a7b270735741
Victor Hang <vhvictorhang@gmail.com>
2025-03-15 23:52:48 +0100

    chore 🧹: remove jelly shim
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/totoro/khoa/config.nix
+++ profiles/totoro/khoa/config.nix
@@ -25,46 +25,6 @@
               Restart = "always";
             };
           };
-          jellyfin-mpv-shim = {
-            Unit = {
-              Description = "Jellyfin mpv";
-            };
-            Install = {
-              WantedBy = [
-                "graphical-session.target"
-                "network-online.target"
-              ];
-            };
-            Unit = {
-              After = [
-                "graphical-session.target"
-                "network-online.target"
-              ];
-            };
-            Service = {
-              ExecStart = "${pkgs.jellyfin-mpv-shim}/bin/jellyfin-mpv-shim";
-              Restart = "always";
-            };
-          };
-          # bealvVpn = {
-          #   Unit = {
-          #     Description = "Bealv VPN";
-          #     After = [
-          #       "graphical-session.target"
-          #       "network-online.target"
-          #     ];
-          #   };
-          #   Install = {
-          #     WantedBy = [
-          #       "graphical-session.target"
-          #       "network-online.target"
-          #     ];
-          #   };
-          #   Service = {
-          #     ExecStart = "${pkgs.bash}/bin/bash -c 'sudo -E ${pkgs.openvpn}/bin/openvpn --up ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --down ${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved --config ${bealvVpnConf} --auth-user-pass $HOME/.bealv_vpn_pass --cd /tmp'";
-          #     Restart = "always";
-          #   };
-          # };
         };
       };
     };
