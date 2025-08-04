2ade986d56c26c3526ed488bee6c3317555a0b11
Victor Hang <vhvictorhang@gmail.com>
2025-08-04 13:48:05 +0200

    feat ✨: global up
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/anya/kubernetes.nix
+++ profiles/anya/kubernetes.nix
@@ -20,24 +20,29 @@ in
     kubectl
     kubernetes
   ];
+  services = {
+    kubernetes = {
+      roles = [
+        "master"
+        "node"
+      ];
+      masterAddress = kubeMasterHostname;
+      apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+      easyCerts = true;
+      apiserver = {
+        securePort = kubeMasterAPIServerPort;
+        advertiseAddress = kubeMasterIP;
+      };
 
-  services.kubernetes = {
-    roles = [
-      "master"
-      "node"
-    ];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    easyCerts = true;
-    apiserver = {
-      securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
+      # use coredns
+      addons.dns.enable = true;
+
+      # needed if you use swap
+      kubelet.extraOpts = "--fail-swap-on=false";
     };
-
-    # use coredns
-    addons.dns.enable = true;
-
-    # needed if you use swap
-    kubelet.extraOpts = "--fail-swap-on=false";
+    nfs.server.enable = true;
+    nfs.server.exports = ''
+      /data/nfs         anya(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
+    '';
   };
 }
