caafa06c5495b6a616e975526b8794431a61fd23
Victor Hang <vhvictorhang@gmail.com>
2025-08-04 13:21:32 +0200

    feat ✨: global up
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

---
+++ profiles/anya/kubernetes.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  # When using easyCerts=true the IP Address must resolve to the master on creation.
+  # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+  kubeMasterIP = "10.207.7.1";
+  kubeMasterHostname = "anya";
+  kubeMasterAPIServerPort = 6443;
+in
+{
+  # resolve master hostname
+  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+
+  # packages for administration tasks
+  environment.systemPackages = with pkgs; [
+    kompose
+    kubectl
+    kubernetes
+  ];
+
+  services.kubernetes = {
+    roles = [
+      "master"
+      "node"
+    ];
+    masterAddress = kubeMasterHostname;
+    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+    easyCerts = true;
+    apiserver = {
+      securePort = kubeMasterAPIServerPort;
+      advertiseAddress = kubeMasterIP;
+    };
+
+    # use coredns
+    addons.dns.enable = true;
+
+    # needed if you use swap
+    kubelet.extraOpts = "--fail-swap-on=false";
+  };
+}
