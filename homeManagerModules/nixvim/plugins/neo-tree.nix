3b2c6188f07f3aefa5a775a5157dbd04a617adb9
Victor Hang <vhvictorhang@gmail.com>
2025-09-18 13:24:37 +0200

    feat ✨: more keybind and shiney
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- homeManagerModules/nixvim/plugins/neo-tree.nix
+++ homeManagerModules/nixvim/plugins/neo-tree.nix
@@ -18,6 +18,25 @@ in
           action = ":Neotree toggle<CR>";
           options.silent = true;
         }
+        {
+          mode = "n";
+          key = "<leader>nt";
+          action.__raw = ''
+            function()
+              local neotree = require("neo-tree.command")
+              local manager = require("neo-tree.sources.manager")
+              local renderer = require("neo-tree.ui.renderer")
+              
+              local state = manager.get_state("filesystem")
+              if state.winid and vim.api.nvim_win_is_valid(state.winid) then
+                local current_width = vim.api.nvim_win_get_width(state.winid)
+                local new_width = current_width <= 25 and 40 or 20
+                vim.api.nvim_win_set_width(state.winid, new_width)
+              end
+            end
+          '';
+          options.silent = true;
+        }
       ];
 
       plugins.neo-tree = {
