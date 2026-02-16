fd264d775d16af1d001a52b87f373a44c2d6fae4
Alan Amoyel <alanamoyel06@gmail.com>
2026-02-16 14:34:41 +0100

    chore 🧹 (nishinoya): change home monitors layout
    Signed-off-by: Alan Amoyel <alanamoyel06@gmail.com>

--- profiles/nishinoya/aamoyel/niriConfig.nix
+++ profiles/nishinoya/aamoyel/niriConfig.nix
@@ -18,5 +18,47 @@ in
         opacity = 0.92;
       }
     ];
+    programs.niri.settings.outputs = lib.mkForce {
+      "*" = {
+        scale = 1.0;
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
+
+      # Configuration for Home bellow
+      "eDP-1" = {
+        scale = 2;
+        position = {
+          x = 3000;
+          y = 0;
+        };
+      };
+      # AOC monitor (center) - using model name for stability
+      "PNP(AOC) 32G1WG4 0x000008DA" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 60.0;
+        };
+        position = {
+          x = 1080;
+          y = 0;
+        };
+      };
+      # BenQ ZOWIE monitor (left, vertical) - using model name for stability
+      "PNP(BNQ) ZOWIE RL LCD X6G00926SL0" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+        };
+        transform.rotation = 90;
+        position = {
+          x = 0;
+          y = 0;
+        };
+      };
+    };
   };
 }
