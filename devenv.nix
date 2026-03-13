bc65b5815f384b9cc5d098134a954c0bd1cc8c69
Victor Hang <vhvictorhang@gmail.com>
2026-03-13 13:36:40 +0100

    feat ✨ (devenv): add devenv configuration and package
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "Welcome to the NixOS configuration environment!";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    colmena
+    npins
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo $GREET
+  '';
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    hello | grep "Welcome"
+  '';
+}
