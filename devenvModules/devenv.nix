2f9310280859f154ec1ac8c8faf5f3e6e768da7a
Victor Hang <vhvictorhang@gmail.com>
2026-03-31 18:49:04 +0200

    refactor 🎨 (devenv): extract git-hooks to separate devenvModules file and restructure treefmt config
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

---
+++ devenvModules/devenv.nix
@@ -0,0 +1,29 @@
+_: {
+  git-hooks.hooks = {
+    shellcheck.enable = true;
+    mdsh.enable = true;
+    treefmt = {
+      enable = true;
+      settings.fail-on-change = false;
+    };
+  };
+
+  difftastic.enable = true;
+  treefmt = {
+    enable = true;
+    config.programs = {
+      nixfmt.enable = true;
+      prettier = {
+        enable = true;
+        excludes = [
+          ".git"
+          ".devenv"
+        ];
+        settings = {
+          proseWrap = "preserve";
+        };
+      };
+      shfmt.enable = true;
+    };
+  };
+}
