81dac96b1ecb413b590dfad16acf531da44205a5
Victor Hang <vhvictorhang@gmail.com>
2026-07-06 09:45:48 +0200

    feat ✨ (dolphinConfig): add thumbnailer and image format support to Dolphin file manager
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- homeManagerModules/dolphinConfig.nix
+++ homeManagerModules/dolphinConfig.nix
@@ -15,6 +15,10 @@ in
       pkgs.kdePackages.ark
       pkgs.kdePackages.kio-admin
       pkgs.kdePackages.ffmpegthumbs
+      pkgs.kdePackages.kdegraphics-thumbnailers
+      pkgs.kdePackages.kio-extras
+      pkgs.kdePackages.kimageformats
+      pkgs.kdePackages.qtimageformats
       pkgs.kdePackages.kpeople
       pkgs.kdePackages.kservice
       pkgs.ntfs3g
@@ -23,9 +27,16 @@ in
 
     xdg.configFile."menus/applications.menu".text =
       builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-    # Ensure dolphin plugins are found
+    # Ensure dolphin plugins and thumbnailers are found
     home.sessionVariables = {
-      QT_PLUGIN_PATH = "${pkgs.kdePackages.dolphin-plugins}/${pkgs.qt6.qtbase.qtPluginPrefix}";
+      QT_PLUGIN_PATH = lib.concatMapStringsSep ":" (p: "${p}/${pkgs.qt6.qtbase.qtPluginPrefix}") [
+        pkgs.kdePackages.dolphin-plugins
+        pkgs.kdePackages.ffmpegthumbs
+        pkgs.kdePackages.kdegraphics-thumbnailers
+        pkgs.kdePackages.kimageformats
+        pkgs.kdePackages.qtimageformats
+        pkgs.kdePackages.kio-extras
+      ];
     };
   };
 }
