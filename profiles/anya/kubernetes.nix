c6b70e6b87abb512070ce649b26082fd7ab65321
Victor Hang <victor@stackhpc.com>
2026-08-12 22:07:16 +0200

    feat: anya k8s

--- profiles/anya/kubernetes.nix
+++ profiles/anya/kubernetes.nix
@@ -78,7 +78,6 @@
   boot.kernel.sysctl = {
     "net.bridge.bridge-nf-call-iptables" = 1;
     "net.bridge.bridge-nf-call-ip6tables" = 1;
-    "net.ipv4.ip_forward" = 1;
   };
   # services = {
   #   kubernetes = {
